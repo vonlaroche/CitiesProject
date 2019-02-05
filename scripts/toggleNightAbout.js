83bae57a53d5039f570e03b52bfe53e7702d5e84
Monika Dominic <dominic.monika@gmail.com>
2019-02-05 17:17:18 +0100

    handled image transitions through css rather than javascript

--- scripts/toggleNightAbout.js
+++ scripts/toggleNightAbout.js
@@ -7,17 +7,9 @@ var headerImageNight = document.querySelector(".headerImageNight");
 nightBtn.addEventListener("click", function(event){
     event.preventDefault();
     body.classList.add("night");
-    headerImage.classList.remove("opaque");
-    headerImage.classList.add("transparent");
-    headerImageNight.classList.remove("transparent");
-    headerImage.classList.add("opaque");
 });
 
 dayBtn.addEventListener("click", function(event){
     event.preventDefault();
     body.classList.remove("night");
-    headerImage.classList.remove("transparent");
-    headerImage.classList.add("opaque");
-    headerImageNight.classList.remove("opaque");
-    headerImageNight.classList.add("transparent");
 });
